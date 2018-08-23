40e4dc7f127443e8194eee391f8a686e6abec486
Isaac <isaachill13@gmail.com>
2018-08-23 19:25:40 +1000

    bye server

--- server/src/server.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/home", (req, res) => res.send({ data: "contact" }));
-
-app.listen(5000, () => console.log("server listening on port 5000"));
