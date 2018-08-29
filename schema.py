92e4329ed861f14f2d7542f56c88d8814c656559
Isaac <isaachill13@gmail.com>
2018-08-29 20:52:53 +1000

    gottem

--- schema.py
+++ schema.py
@@ -3,19 +3,32 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField
-from data.user.user_schema import UserConnections
+from data.user.user_schema import UserConnections, User
 from data.menu.menu_schema import MenuConnections
 from data.recipe.recipe_schema import RecipeConnections
 # import required to know about ingredients field - don't think i like this
 from data.ingredient.ingredient_schema import IngredientConnections
 
+class MyConnectionField(SQLAlchemyConnectionField):
+    RELAY_ARGS = ['first', 'last', 'before', 'after']
 
+    @classmethod
+    def get_query(cls, model, info, **args):
+        query = super(MyConnectionField, cls).get_query(model, info, **args)
+        for field, value in args.items():
+            if field not in cls.RELAY_ARGS:
+                query = query.filter(getattr(model, field) == value)
+        return query
+
+# class Query(graphene.ObjectType):
+#     node = graphene.relay.Node.Field()
+#     all_somethings = MyConnectionField(SomethingNode) 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     # Allows sorting over multiple columns, by default over the primary key
     all_menus = SQLAlchemyConnectionField(MenuConnections, sort=None)
     all_recipes = SQLAlchemyConnectionField(RecipeConnections, sort=None)
-    all_users = SQLAlchemyConnectionField(UserConnections, sort=None)
+    all_users = MyConnectionField(UserConnections, email=graphene.String(), name=graphene.String(), sort=None)
 
 
 schema = graphene.Schema(query=Query)
