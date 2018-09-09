59b89fcce6c3e138d382e80a5f95f11335573dd6
Isaac <isaachill13@gmail.com>
2018-09-09 16:05:48 +1000

    added thingo

---
+++ clean_prod_db_info.py
@@ -0,0 +1,16 @@
+import os
+from data.ingredient.ingredient_model import Ingredient
+from data.recipe.recipe_model import Recipe
+from data.menu.menu_model import Menu
+from data.user.user_model import User
+from data.base import Base, db_session, engine, association_table
+
+Ingredient.__table__.drop(engine)
+Recipe.__table__.drop(engine)
+Menu.__table__.drop(engine)
+User.__table__.drop(engine)
+association_table.drop(engine)
+db_session.commit()
+
+Base.metadata.create_all(bind=engine)
+db_session.commit()
