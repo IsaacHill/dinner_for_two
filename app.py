372df0a1539b9776d964e6973e0e7e31b9919095
Isaac <isaachill13@gmail.com>
2018-09-30 16:45:00 +1000

    fix #3 cors issue, that is currently effecting the setup of the app

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask import Flask
 from config import JWT_SECRET_KEY
 from flask_graphql import GraphQLView
 from flask_graphql_auth import GraphQLAuth
-
+from flask_cors import CORS
 from data.base import db_session
 from data.schema import schema
+import os
 
 app = Flask(__name__)
 auth = GraphQLAuth(app)
@@ -25,6 +26,7 @@ app.add_url_rule(
     ),
 )
 
+cors = CORS(app, resources={"*": {"origins": os.environ['DOMAIN']}})
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
